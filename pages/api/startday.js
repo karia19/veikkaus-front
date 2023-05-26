059989dc2f3f26aefb7916fda7e723b5d8d5b9e2
karia19 <39730014+karia19@users.noreply.github.com>
2023-05-26 13:19:45 +0300

    Update startday.js

--- pages/api/startday.js
+++ pages/api/startday.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 export async function getData() {
-    const response = await axios.get('http://localhost:8000/api/v1/toto')
+    //const response = await axios.get('http://localhost:8000/api/v1/toto')
+    const response = await axios.get('https://server-toto.onrender.com/api/v1/toto')
+    
     const jsonData = await response.json()
     return jsonData
 }
